590f2b530735f3da5f20341bc44c1e2b51f7cf1c
XLJuice <35495987+XLJuice@users.noreply.github.com>
2018-11-22 23:19:13 +0200

    Add files via upload

---
+++ Lab7/Lab7/fMain.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Lab7
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbX1 = new System.Windows.Forms.TextBox();
+            this.tbX2 = new System.Windows.Forms.TextBox();
+            this.tbY = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.avg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Змінна X1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Змінна X2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Середнє арифметичне";
+            this.label4.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // tbX1
+            // 
+            this.tbX1.Location = new System.Drawing.Point(148, 6);
+            this.tbX1.Name = "tbX1";
+            this.tbX1.Size = new System.Drawing.Size(100, 20);
+            this.tbX1.TabIndex = 3;
+            this.tbX1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tbX2
+            // 
+            this.tbX2.Location = new System.Drawing.Point(148, 33);
+            this.tbX2.Name = "tbX2";
+            this.tbX2.Size = new System.Drawing.Size(100, 20);
+            this.tbX2.TabIndex = 4;
+            this.tbX2.TextChanged += new System.EventHandler(this.tbX2_TextChanged);
+            // 
+            // tbY
+            // 
+            this.tbY.Location = new System.Drawing.Point(148, 60);
+            this.tbY.Name = "tbY";
+            this.tbY.ReadOnly = true;
+            this.tbY.Size = new System.Drawing.Size(100, 20);
+            this.tbY.TabIndex = 5;
+            this.tbY.TextChanged += new System.EventHandler(this.tbY_TextChanged);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 129);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Обчислити";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClean
+            // 
+            this.btnClean.Location = new System.Drawing.Point(93, 129);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(75, 23);
+            this.btnClean.TabIndex = 7;
+            this.btnClean.Text = "Очистити";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(174, 129);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Вихiд";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Результат розрахунку Y";
+            // 
+            // avg
+            // 
+            this.avg.Location = new System.Drawing.Point(148, 89);
+            this.avg.Name = "avg";
+            this.avg.ReadOnly = true;
+            this.avg.Size = new System.Drawing.Size(100, 20);
+            this.avg.TabIndex = 10;
+            this.avg.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 165);
+            this.Controls.Add(this.avg);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.tbY);
+            this.Controls.Add(this.tbX2);
+            this.Controls.Add(this.tbX1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторна робота 7";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbX1;
+        private System.Windows.Forms.TextBox tbX2;
+        private System.Windows.Forms.TextBox tbY;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox avg;
+    }
+}
+
---
+++ Lab7/Lab7/fMain.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbX1.Text) ||
+               (String.IsNullOrEmpty(tbX2.Text)))
+            {
+                tbY.Text = "Не введено даних!";
+                return;
+            }
+            double x1 = double.Parse(tbX1.Text);
+            double x2 = double.Parse(tbX2.Text);
+            double y = (Math.Sqrt(Math.Pow(x1,3)+Math.Pow(x2,5)))/(1000*Math.Sqrt(x1+Math.Pow(x2,5)))+65;
+            tbY.Text = y.ToString("0.######");
+            double z = (x1 + x2) / 2;
+            avg.Text = z.ToString("0.######");
+        }
+
+        private void tbY_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbX2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            tbX1.Text = string.Empty;
+            tbX2.Text = string.Empty;
+            tbY.Text = string.Empty;
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
